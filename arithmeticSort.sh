230ae0181d24a3df84b6970f4046fa127c810764
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-11-08 16:29:48 +0530

    Added : Code for storing results in Dictionary.

--- arithmeticSort.sh
+++ arithmeticSort.sh
@@ -11,3 +11,16 @@ no1=`expr $a + $b \* $c`
 no2=`expr $a \* $b + $c`
 no3=`expr $c + $a / $b`
 no4=`expr $a % $b + $c`
+
+declare -A results
+results[no1]=$no1
+results[no2]=$no2
+results[no3]=$no3
+results[no4]=$no4
+
+for i in ${!results[@]}
+do
+echo $i "." ${results[$i]}
+done
+
+
